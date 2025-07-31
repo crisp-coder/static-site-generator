ec14fe29d1dc9ff3d09c227d1733f66fdfca9fe1
crisp-coder <mehrunesdagon737@gmail.com>
2025-07-31 03:21:44 -0500

    Added python server, Added TextNode, Added unittest

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    print("hello world")
+    node = TextNode("This is some anchor text", "link", "https://github.com")
+    print(node)
+
+if __name__ == "__main__":
+    main()
