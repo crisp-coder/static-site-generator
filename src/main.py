f23e918588cdae888195f0c19a6c5550c5dabcd0
crisp-coder <mehrunesdagon737@gmail.com>
2025-08-03 07:16:23 -0500

    Working SSG for local and github pages.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ from page_builder import generate_page
 from shutil import rmtree, copytree
 from os.path import abspath, exists, isdir, join, isfile, relpath
 from os import listdir, mkdir
+from sys import argv
 
 def get_file_paths(directory, files):
+    # Build a list of files in the content
+    # directory and subdirectories
+    # Output is saved in files parameter
     if exists(directory):
         for entry in listdir(directory):
             full_path = join(directory, entry)
@@ -11,9 +15,22 @@ def get_file_paths(directory, files):
                 files.append(full_path)
             elif isdir(full_path):
                 get_file_paths(full_path, files)
+
 def main():
-    public_dir = "public"
+    # Base path is a string to use when hosting the site on github pages.
+    base_path = '/'
+    if len(argv) == 2:
+        base_path = argv[1]
+    print(f'base_path is {base_path}')
+
+    # public_dir is the directory to store the generated site.
+    public_dir = "docs"
+    # content dir is the directory the markdown files are stored.
     content_dir = "content"
+    # static dir is the directory the static assets files are stored.
+    static_dir = "static"
+
+    # Delete and remake public directory
     if isdir(abspath(public_dir)):
         rmtree(abspath(public_dir))
     mkdir(abspath(public_dir))
@@ -23,14 +40,20 @@ def main():
 
     for file in content_files:
         print(f'{file}')
-        pruned_file = file[len(content_dir)+1:-2]
-        pruned_file += 'html'
-        generate_page(file, "template.html", abspath(join(abspath(public_dir), pruned_file)))
+        dest_path = abspath(public_dir)
+        print(f'dest_path = {dest_path}')
+        generate_page(
+            file,
+            "template.html",
+            dest_path,
+            base_path,
+            content_dir
+        )
 
-    static_dir = "static"
+    # Copy the static assets to the public folder.
     if exists(abspath(static_dir)):
-        print(f'Copying {static_dir} to {public_dir}')
-        copytree(static_dir, abspath(public_dir), dirs_exist_ok=True)
+        public_base_path = abspath(public_dir) + base_path
+        copytree(static_dir, public_base_path, dirs_exist_ok=True)
 
 if __name__ == "__main__":
     main()
--- src/page_builder.py
+++ src/page_builder.py
@@ -1,43 +1,45 @@
 from markdown import markdown_to_html_node
 from markdown import extract_markdown_title
-from os.path import abspath, exists, isfile, isdir, dirname
+from os.path import join, abspath, isfile, dirname
 from os import makedirs
 
-def generate_page(from_path, template_path, dest_path):
-    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
-
+def generate_page(from_path, template_path, dest_path, base_path, content_dir):
     if not isfile(abspath(from_path)):
         raise Exception("Provided mardown filepath is not a file.")
     if not isfile(abspath(template_path)):
         raise Exception("Provided template filepath is not a file.")
-    if dest_path != "":
-        makedirs(dirname(abspath(dest_path)), mode=0o777, exist_ok=True)
+    if dest_path == "":
+        raise Exception("No destination path provided.")
+    if not from_path.endswith(".md"):
+        raise Exception(f"{from_path} is not a markdown file.")
 
     markdown = ""
     with open(abspath(from_path), 'r') as source_file:
         markdown = source_file.read()
-
-    #print(f'{markdown}')
-    if markdown == "":
+    if len(markdown) == 0:
         raise Exception("Markdown file is empty.")
 
     # Parse Markdown into HTML
     title = extract_markdown_title(markdown)
     html_node = markdown_to_html_node(markdown)
     html_str = html_node.to_html()
-    #print(f'{html_str}')
 
     with open(abspath(template_path), 'r') as template_file:
         template = template_file.read()
 
-    #print(f'{template}')
-    # Insert html content into Template.
+    # Insert html content into template.
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_str)
+    # Update links and images to point to base path.
+    template = template.replace('href="/', f'href="{base_path}')
+    template = template.replace('src="/', f'src="{base_path}')
 
-    #print(f'{template}')
-    # Write out produced html file.
-    with open(abspath(dest_path), 'w') as output_file:
+    # Create output file path.
+    html_file = from_path[len(content_dir):-2] + 'html'
+    output_file_path = abspath(dest_path) + base_path + html_file
+    print(f'output_file_path = {output_file_path}')
+
+    # Create subdirectories based on output file path hierarchy.
+    makedirs(dirname(output_file_path), mode=0o777, exist_ok=True)
+    with open(abspath(output_file_path), 'w') as output_file:
         output_file.write(template)
-    print(f'wrote template to {abspath(dest_path)}')
-    print(f'Done')
